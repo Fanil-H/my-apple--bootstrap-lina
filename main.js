7bb4e6b6f5e4c211b6d04078483854fb57362e08
linaa-khairutdinova <springitschool3@gmail.com>
2023-06-09 23:45:57 +0300

    my apple lina

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//   $(".slider").bxSlider({
+//     auto: true,
+//     autoControls: true,
+//     stopAutoOnClick: true,
+//     pager: false,
+//     slideWidth: 1100
+//   });
+// });
